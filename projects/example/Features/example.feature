27df8960313f3522dd175ad32118bb739f11af08
xausee <xausee@gmail.com>
2014-05-07 17:54:39 +0800

    rename folder

--- projects/example/Features/example.feature
+++
@@ -1,6 +0,0 @@
-Feature:Test WebTestCraft framework.
-
-Scenario: Visit baidu and do a search
-  Given the user visit baidu home page
-  And search game using keyword Dota
-  Then get a search result page
--- projects/example/Features/support/env.rb
+++
@@ -1,8 +0,0 @@
-$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
-$CUCUMBER_CURRENT_PATH =  Dir.pwd
-$ROOT_PATH = File.expand_path(File.dirname(__FILE__)) + '/../../../../'
-$LOAD_PATH.unshift($ROOT_PATH)
-require 'core/load_framework.rb'
-
-
-
